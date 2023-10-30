b2cd5e6dacf0014e1038cf9ee24a921a25909aff
Rick Arcanjo <ricardo.cassettari07@gmail.com>
2023-10-29 23:42:36 -0300

    exercicio realizado

---
+++ script.js
@@ -0,0 +1,21 @@
+let nota1 = prompt('Digite a primeira nota...');
+let nota2 = prompt('Digite a segunda nota....');
+let nota3 = prompt('Digite a terceira nota...');
+let nota4 = prompt('Digite a quarta nota.....');
+let somaNotas = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4));
+
+mediaNotas = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4)) / 4;
+
+if (somaNotas < 0 || somaNotas > 40) {
+    alert('Verifique as notas que foram colocadas.');
+}
+
+else if (mediaNotas < 6) {
+    alert('Reprovado!');
+}
+
+else if (mediaNotas >= 6) {
+    alert('Aprovado!');
+} 
+
+alert(`A média entre as notas foi: ${mediaNotas}`)
